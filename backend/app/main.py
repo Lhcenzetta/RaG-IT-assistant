c4a5c1bbd7d3cb043aa621834b4ca8eedae243e9
lait-zet <lhcenztta18@gmail.com>
2026-01-31 23:01:20 +0100

    set config for fastapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,5 +2,11 @@ from fastapi import FastAPI
 from db.models import Base
 from db.database import engine
 from api  import auth 
+
+
 Base.metadata.create_all(engine)
+
 app = FastAPI()
+
+
+app.include_router(auth.router)
